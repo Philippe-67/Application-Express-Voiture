0a091031ecf1fdcfea6ce30f8ddd3ee663d49f41
Philippe CHARLES <philippe.charles@axa.fr>
2023-05-29 20:51:29 +0200

    mise en place des tables voitures et réparations, avec accès dans la barre de navigation

---
+++ Application Express Voiture/Data/Migrations/20230529184925_MVCVoitureReparation.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Application_Express_Voiture.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MVCVoitureReparation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Voiture",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Marque = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modele = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PrixAchat = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Voiture", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reparation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TypeReparation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PrixDeLaReparation = table.Column<float>(type: "real", nullable: false),
+                    VoitureId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reparation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reparation_Voiture_VoitureId",
+                        column: x => x.VoitureId,
+                        principalTable: "Voiture",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reparation_VoitureId",
+                table: "Reparation",
+                column: "VoitureId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reparation");
+
+            migrationBuilder.DropTable(
+                name: "Voiture");
+        }
+    }
+}
